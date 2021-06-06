1b2e7db8660e84b12433771f1fbb4c264f6d130b
mirbek <mirbek.25.52@gmail.com>
2021-06-06 18:29:45 +0600

    Updates

---
+++ precache-manifest.39d84530465a3119bef042844c6d6118.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76fb366cfea53d27e36a823870504b28",
+    "url": "/cocktail/index.html"
+  },
+  {
+    "revision": "30a0485b6366142b9f75",
+    "url": "/cocktail/static/css/main.67a2c7ec.chunk.css"
+  },
+  {
+    "revision": "e1f6309a6f0d3e350015",
+    "url": "/cocktail/static/js/2.72303b8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cocktail/static/js/2.72303b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30a0485b6366142b9f75",
+    "url": "/cocktail/static/js/main.ee6377de.chunk.js"
+  },
+  {
+    "revision": "010f58731b5c0aef6539",
+    "url": "/cocktail/static/js/runtime-main.c98f1fc0.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/cocktail/static/media/logo.9a3d2645.svg"
+  },
+  {
+    "revision": "2ea4c43614e5b0beff1d450e9d280ea2",
+    "url": "/cocktail/static/media/search.2ea4c436.svg"
+  }
+]);
